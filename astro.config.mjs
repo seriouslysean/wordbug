2f0984fd654bec8890ac9049054cfb8e4c53467f
Sean Kennedy <sean@codeserious.com>
2025-04-14 10:18:42 -0400

    Remove config, set up base components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,12 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-
-const site = process.env.BASE_URL || 'http://localhost:4321';
-const base = process.env.BASE_PATH || '';
 
 // https://astro.build/config
 export default defineConfig({
-    site,
-    base,
-    output: 'static',  // Explicitly set static output
+    site: 'https://wordbug.fyi',
+    base: '/',
+    output: 'static',
     devToolbar: {
         enabled: false
     },
-    build: {
-        assets: 'assets'  // Put assets in a dedicated directory
-    },
-    vite: {
-        resolve: {
-            alias: {
-                '~': '/src',
-                '~components': '/src/components',
-                '~layouts': '/src/layouts',
-                '~utils': '/src/utils',
-                '~data': '/src/data',
-                '~styles': '/src/styles',
-                '~config': '/src/config'
-            }
-        }
-    }
 });
